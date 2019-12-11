87710b8fd9ea57ba087166623e86b80794b76b2f
SergioUnix <2416465290108@ingenieria.usac.edu.gt>
2019-12-11 00:12:30 -0600

    Menu creado

--- Proyecto1_EDD/main.cpp
+++ Proyecto1_EDD/main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+
+    int a, b;
+    do{
+
+
+        cout << "1- saludar \n";
+        cout << "2- retiro \n";
+        cout << "3- salir \n \n";
+        cin>>a;
+
+        switch (a)
+        {
+           case 1:
+           cout <<" hola te mando saludo \n";
+           break;
+           case 2:
+           cout <<" hola me retiro \n";
+           break;
+           case 3:
+
+                cout <<" Seguro que quieres ...?  SI o NO \n";
+                cout <<"\n 1-SI \n";
+                cout <<"\n 2-NO \n";
+                cin>>b;
+                if(b==1)
+                {
+                    return 0;
+                }else{
+                    return main();
+                }
+                break;
+           default:
+                return main();
+                break;
+           }
+        }
+        while (a != 3);
+        system("pause");
+        return main();
+
 }
+
+
